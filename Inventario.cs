09b294b225d8e951583b9949e60f6342b1a2eea7
melmontes27 <melmontes.cruz@gmail.com>
2026-04-16 08:02:18 -0600

    Update Inventario.cs

--- Inventario.cs
+++ Inventario.cs
@@ -1,1 +1,25 @@
+using System;
 
+public class Inventario()
+{
+}
+public void AgregarProducto(nombre,stock)
+{
+  
+}
+public void ListarProductos()
+{
+  
+}
+public void BuscarProducto(nombre)
+{
+  
+}
+public void ActualizarStock(id,nuevoStock)
+{
+  
+}
+public void EliminarProducto(id)
+{
+  
+}
